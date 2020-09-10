4201a327ea629dd6a75392b9ff76e1eed2756fb8
Данил Филиппов <sniper9543@yandex.ru>
2020-09-10 20:32:10 +0300

    HW 2.0 ver.1.0

---
+++ posts/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Post, Group
+
+
+class PostAdmin(admin.ModelAdmin):
+    # перечисляем поля, которые должны отображаться в админке
+    list_display = ("pk","text", "pub_date", "author", "group") 
+    # добавляем интерфейс для поиска по тексту постов
+    search_fields = ("text",) 
+    # добавляем возможность фильтрации по дате
+    list_filter = ("pub_date",) 
+    empty_value_display = "-пусто-"
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ("pk","title","slug","description")
+    search_fields = ("text",) 
+    empty_value_display = "-пусто-"
+
+admin.site.register(Post, PostAdmin) 
+admin.site.register(Group, GroupAdmin) 
