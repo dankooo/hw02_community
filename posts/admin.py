a688103c15c91ca58173f99a8b1c6ec9e45e2f5c
Данил Филиппов <sniper9543@yandex.ru>
2020-09-17 18:20:18 +0300

    New try ver. 2.0

--- posts/admin.py
+++ posts/admin.py
@@ -1,20 +1,21 @@
 from django.contrib import admin
-from .models import Post, Group
+
+from .models import Post
+from .models import Group
 
 
 class PostAdmin(admin.ModelAdmin):
-    # перечисляем поля, которые должны отображаться в админке
-    list_display = ("pk","text", "pub_date", "author", "group") 
-    # добавляем интерфейс для поиска по тексту постов
-    search_fields = ("text",) 
-    # добавляем возможность фильтрации по дате
-    list_filter = ("pub_date",) 
+    list_display = ("pk", "text", "pub_date", "author", "group") 
+    search_fields = ("text") 
+    list_filter = ("pub_date") 
     empty_value_display = "-пусто-"
 
+
 class GroupAdmin(admin.ModelAdmin):
-    list_display = ("pk","title","slug","description")
-    search_fields = ("text",) 
+    list_display = ("pk", "title", "slug", "description")
+    search_fields = ("text") 
     empty_value_display = "-пусто-"
+
 
 admin.site.register(Post, PostAdmin) 
 admin.site.register(Group, GroupAdmin) 
--- posts/urls.py
+++ posts/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
-    path("group/<str:slug>", views.group_posts)
+    path("group/<slug>", views.group_posts, name=slug)
 ]
